5c7bd25ba5978df98dfab2f5949d4f0f984cfb28
Maristo-10 <maristositoruz@gmail.com>
2025-06-29 15:47:27 +0700

    update

--- resources/views/app/pages/about_us/index.blade.php
+++ resources/views/app/pages/about_us/index.blade.php
@@ -25,18 +25,18 @@
 
     <section class="layout-pt-md pb-60">
         <div class="container">
-            <div class="row y-gap-30 justify-between items-center">
+            <div class="row y-gap-30 justify-between">
                 <div class="col-lg-5">
+                    <img src="/uploads/about_us/{{ $about_us->image_path }}" alt="image" class="rounded-4"
+                        style="width: -webkit-fill-available;">
+                </div>
+                <div class="col-lg-7">
                     <h2 class="text-30 fw-600">Blessing89 Tour Travel</h2>
                     <p class="mt-5">{{ $about_us->title }}</p>
 
                     <p class="text-dark-1 mt-40 lg:mt-40 md:mt-20">
                         {!! $about_us->description !!}
                     </p>
-                </div>
-
-                <div class="col-lg-6">
-                    <img src="/uploads/about_us/{{ $about_us->image_path }}" alt="image" class="rounded-4" style="width: -webkit-fill-available;">
                 </div>
             </div>
         </div>
